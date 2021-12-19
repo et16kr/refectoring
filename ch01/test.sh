8648a197f45fa0822eca84ce23b054c3d52902ac
jungkeun <et16kr@gmail.com>
2021-12-19 09:40:56 +0900

    update test.sh

--- ch01/test.sh
+++ ch01/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+rm output.txt
 node example.js > output.txt
 export result=$(diff current.txt output.txt)
 if [ -z "${result}" ]; then
     echo "Test Pass"
-    cat current.txt 
+    cat output.txt
 else
     echo "Test Fail"
     echo -e "${result}"
